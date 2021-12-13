9590f113de645937acf96dab7f267b4934a285e8
DimasPradana <deemaazbaek@gmail.com>
2021-12-13 09:21:38 +0700

    migrate keymaps to which-key

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -35,64 +35,9 @@ nnoremap("N", "Nzzzv")
 exprnnoremap("k", "v:count == 0 ? 'gk' : 'k'")
 exprnnoremap("j", "v:count == 0 ? 'gj' : 'j'") -- }}}
 
--- {{{ telescope
-nnoremap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
-nnoremap("<leader>fg", "<cmd>lua require('telescope.builtin').grep_string()<cr>") -- get string on cursor at current working directory
-nnoremap("<leader>fw", "<cmd>lua require('telescope.builtin').live_grep()<cr>") -- get file from text input on current working directory
-nnoremap("<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>") -- get list of buffers
-nnoremap("<leader>fc", "<cmd>lua require('telescope.builtin').commands()<cr>") -- Lists available plugin/user commands and runs them on <cr>
-nnoremap("<leader>fk", "<cmd>lua require('telescope.builtin').keymaps()<cr>") -- Lists normal mode keymappings
-nnoremap("<leader>fr", "<cmd>lua require('telescope.builtin').registers()<cr>") -- Lists registers
-nnoremap("<leader>fh", "<cmd>lua require('telescope.builtin').highlights()<cr>") -- Lists highlights
-nnoremap("<leader>fch", "<cmd>lua require('telescope.builtin').command_history()<cr>") -- Lists command_history
-nnoremap("<leader>fsh", "<cmd>lua require('telescope.builtin').search_history()<cr>") -- Lists search_history
-nnoremap("<leader>fmp", "<cmd>lua require('telescope.builtin').man_pages()<cr>") -- Lists manual pages
-nnoremap("<leader>fm", "<cmd>lua require('telescope.builtin').marks()<cr>") -- Lists marks
--- telescope LSP
-nnoremap("<leader>lr", "<cmd>lua require('telescope.builtin').lsp_references()<cr>") -- Lists LSP references for word under the cursor
-nnoremap("<leader>lds", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>") -- Lists LSP document symbols in the current buffer
-nnoremap("<leader>lws", "<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<cr>") -- Lists LSP document symbols in the current workspace
-nnoremap("<leader>ldw", "<cmd>lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<cr>") -- Dynamically Lists LSP for all workspace symbols
-nnoremap("<leader>lca", "<cmd>lua require('telescope.builtin').lsp_code_actions()<cr>") -- Lists any LSP actions for the word under the cursor, that can be triggered with <cr>
--- nnoremap("<leader>lca", "<cmd>lua require('telescope.builtin').lsp_range_code_actions()<cr>") -- Lists any LSP actions for a given range, that can be triggered with <cr>
-nnoremap("<leader>ldd", "<cmd>lua require('telescope.builtin').lsp_document_diagnostics()<cr>") -- Lists LSP diagnostics for the current buffer
-nnoremap("<leader>lwd", "<cmd>lua require('telescope.builtin').lsp_workspace_diagnostics()<cr>") -- Lists LSP diagnostics for the current workspace if supported, otherwise searches in all open buffers
-nnoremap("<leader>lim", "<cmd>lua require('telescope.builtin').lsp_implementations()<cr>") -- Goto the implementation of the word under the cursor if there's only one, otherwise show all options in Telescope
-nnoremap("<leader>lde", "<cmd>lua require('telescope.builtin').lsp_definitions()<cr>") -- Goto the definition of the word under the cursor, if there's only one, otherwise show all options in Telescope
-nnoremap("<leader>ltd", "<cmd>lua require('telescope.builtin').lsp_type_definitions()<cr>") -- Goto the definition of the word under the cursor, if there's only one, otherwise show all options in Telescope
--- telescope treesitter
-nnoremap("<leader>tre", "<cmd>lua require('telescope.builtin').treesitter()<cr>") -- Lists Function names, variables, from Treesitter!
--- telescope emoji
-nnoremap("<leader>emo", "<cmd>lua require'telescope.builtin'.symbols{ sources = {'emoji', 'kaomoji', 'gitmoji'} }<cr>") -- telescope emoji doong 😀
--- }}}
-
--- {{{ nvim-tree
-nnoremap("<C-n>", "<cmd>NvimTreeToggle<cr>")
-nnoremap("<leader>r", "<cmd>NvimTreeRefresh<cr>") -- }}}
-
--- {{{ hop
-nnoremap("<leader>hw", "<cmd>lua require('hop').hint_words()<cr>") -- hint words
-nnoremap("<leader>hp", "<cmd>lua require('hop').hint_patterns()<cr>") -- hint pattern
-nnoremap("<leader>hl", "<cmd>lua require('hop').hint_lines()<cr>") -- hint line
-nnoremap("<leader>hc", "<cmd>lua require('hop').hint_char2()<cr>") -- hint char -- }}}
-
 -- {{{ bufferline
 nnoremap("<A-l>", "<cmd>BufferLineCycleNext<cr>") -- next
 nnoremap("<A-h>", "<cmd>BufferLineCyclePrev<cr>") -- previous -- }}}
-
--- {{{ packer
-nnoremap("<leader>ps", "<cmd>PackerSync<cr>") -- packer sync
-nnoremap("<leader>pc", "<cmd>PackerCompile<cr>") -- packer compile -- }}}
-
--- {{{ node
-nnoremap("<leader>nod", "<cmd>!tmux split-window 'npm run dev'; read;<cr>") -- svelte run development
-nnoremap("<leader>nop", "<cmd>!node build/index.js<cr>") -- node build index js -- }}}
-
--- {{{ lua
-nnoremap("<leader>lua", "<cmd>echo '｡:.ﾟヽ(*´∀`)ﾉﾟ.:｡' | luafile %<cr>") -- source lua -- }}}
-
--- {{{ formatter
-nnoremap("<leader>n", "<cmd>Neoformat<cr>") -- run neoformat -- }}}
 
 -- {{{ searchbox
 -- nnoremap("/", "<cmd>lua require('searchbox').match_all({clear_matches = true})<cr>") -- run searchbox -- }}}
--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -9,3 +9,125 @@ wk.setup({
 		winblend = 0.5,
 	},
 })
+
+-- {{{ lua and lsp
+wk.register({
+	["<leader>"] = {
+		l = {
+			name = "Lua and LSP",
+			u = { "<CMD>echo '｡:.ﾟヽ(*´∀`)ﾉﾟ.:｡' | luafile %<CR>", "source lua" },
+			r = { "<CMD>lua require('telescope.builtin').lsp_references()<CR>", "lsp references" },
+			s = {
+				name = "lsp symbols",
+				d = { "<CMD>lua require('telescope.builtin').lsp_document_symbols()<CR>", "lsp doc symbols" },
+				w = { "<CMD>lua require('telescope.builtin').lsp_workspace_symbols()<CR>", "lsp workspace symbols" },
+				y = {
+					"<CMD>lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<CR>",
+					"lsp dynamic workspace symbols",
+				},
+			},
+			c = { "<CMD>lua require('telescope.builtin').lsp_code_actions()<CR>", "lsp code actions" },
+			d = {
+				name = "lsp diagnostic", -- FIXME deprecated Please use `Telescope diagnostics`.For more information see `:help telescope.changelog-1553`
+				d = { "<CMD>lua require('telescope.builtin').lsp_document_diagnostics()<CR>", "lsp doc diag" },
+				w = { "<CMD>lua require('telescope.builtin').lsp_workspace_diagnostics()<CR>", "lsp work diag" },
+			},
+			i = { "<CMD>lua require('telescope.builtin').lsp_implementations()<CR>", "lsp implementation" },
+			e = {
+				name = "lsp definitions",
+				e = { "<CMD>lua require('telescope.builtin').lsp_definitions()<CR>", "lsp definitions" },
+				t = { "<CMD>lua require('telescope.builtin').lsp_type_definitions()<CR>", "lsp type definitions" },
+			},
+		},
+	},
+}) -- }}}
+
+-- {{{ telescope
+wk.register({
+	["<leader>"] = {
+		f = {
+			name = "Telescope",
+			f = { "<CMD>lua require('telescope.builtin').find_files()<CR>", "find files" },
+			g = { "<CMD>lua require('telescope.builtin').grep_string()<CR>", "grep string" },
+			w = { "<CMD>lua require('telescope.builtin').live_grep()<CR>", "live grep" },
+			b = { "<CMD>lua require('telescope.builtin').buffers()<CR>", "buffers" },
+			c = { "<CMD>lua require('telescope.builtin').commands()<CR>", "commands" },
+			k = { "<CMD>lua require('telescope.builtin').keymaps()<CR>", "keymaps" },
+			r = { "<CMD>lua require('telescope.builtin').registers()<CR>", "register" },
+			h = { "<CMD>lua require('telescope.builtin').highlights()<CR>", "highlight" },
+			i = {
+				name = "history",
+				c = { "<CMD>lua require('telescope.builtin').command_history()<CR>", "command history" },
+				h = { "<CMD>lua require('telescope.builtin').search_history()<CR>", "search history" },
+			},
+			m = {
+				name = "mark and man pages",
+				a = { "<CMD>lua require('telescope.builtin').marks()<CR>", "marks" },
+				m = { "<CMD>lua require('telescope.builtin').man_pages()<CR>", "man pages" },
+			},
+			t = { "<CMD>lua require('telescope.builtin').treesitter()<CR>", "telescope > treesitter" }, -- Lists Function names, variables, from Treesitter!
+			e = {
+				"<CMD>lua require'telescope.builtin'.symbols{ sources = {'emoji', 'kaomoji', 'gitmoji'} }<CR>",
+				"telescope emoji",
+			}, -- emoji dong 😀
+		},
+	},
+}) -- }}}
+
+-- {{{ hop
+wk.register({
+	["<leader>"] = {
+		h = {
+			name = "Hop",
+			w = { "<CMD>lua require('hop').hint_words()<CR>", "hint words" },
+			p = { "<CMD>lua require('hop').hint_patterns()<CR>", "hint patterns" },
+			l = { "<CMD>lua require('hop').hint_lines()<CR>", "hint lines" },
+			c = { "<CMD>lua require('hop').hint_char2()<CR>", "hint chars" },
+		},
+	},
+}) -- }}}
+
+-- {{{ node
+wk.register({
+	["<leader>"] = {
+		o = {
+			name = "Node",
+			i = { "<CMD>lua require('node').npm_install()<CR>", "npm install" },
+			r = { "<CMD>!tmux split-window 'npm run dev'; read;<CR>", "npm run dev" }, -- svelte run develepment
+			b = { "<CMD>!node build/index.js<CR>", "npm build" }, -- npm build / svelte build
+		},
+	},
+}) -- }}}
+
+-- {{{ nvimtree
+wk.register({
+	["<leader>"] = {
+		e = {
+			name = "Nvimtree",
+			e = { "<CMD>NvimTreeToggle<CR>", "Toggle" },
+			r = { "<CMD>NvimTreeRefresh<CR>", "Refresh" },
+		},
+	},
+}) -- }}}
+
+-- {{{ formatter
+wk.register({
+	["<leader>"] = {
+		n = { "<CMD>Neoformat<CR>", "Neoformat" }, -- run neoformat
+	},
+}) -- }}}
+
+-- {{{ packer
+wk.register({
+	["<leader>"] = {
+		p = {
+			name = "Packer",
+			s = { "<CMD>PackerSync<CR>", "Packer Sync" },
+			c = { "<CMD>PackerCompile<CR>", "Packer Compile" },
+			i = { "<CMD>PackerInstall<CR>", "Packer Install" },
+			l = { "<CMD>PackerClean<CR>", "Packer Clean" },
+		},
+	},
+}) -- }}}
+
+-- vim:fileencoding=utf-8:ft=lua:foldmethod=marker
