daa6baaab85345fd87a45cfa3cb42de115cb7edd
DimasPradana <deemaazbaek@gmail.com>
2021-12-16 15:38:01 +0700

    add command for lsp debugging

--- init.lua
+++ init.lua
@@ -3,4 +3,6 @@ require("keymaps") -- Keymapping
 require("options") -- Options
 require("ui") -- UI
 
+-- vim.lsp.set_log_level("debug") -- for debugging active
+
 -- vim:fileencoding=utf-8:ft=lua:foldmethod=marker
