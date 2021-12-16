d1c243c30a8700d73c238f15559c4ab32e49efe8
DimasPradana <deemaazbaek@gmail.com>
2021-12-16 11:59:04 +0700

    edit gitsigns config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,15 +8,39 @@ require("gitsigns").setup({
 		topdelete = { hl = "SignDelete", text = "🭶" },
 		changedelete = { hl = "SignChange", text = "▎" },
 	},
+	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 	watch_gitdir = {
 		interval = 1000,
+		follow_files = true,
 	},
-	current_line_blame = true,
-	sign_priority = 5,
-	update_debounce = 500,
+	attach_to_untracked = true,
+	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		delay = 1000,
+		ignore_whitespace = false,
+	},
+	current_line_blame_formatter_opts = {
+		relative_time = false,
+	},
+	sign_priority = 6,
+	update_debounce = 100,
 	status_formatter = nil, -- Use default
-	diff_opts = {
-		internal = true,
+	max_file_length = 40000,
+	preview_config = {
+		-- Options passed to nvim_open_win
+		border = "single",
+		style = "minimal",
+		relative = "cursor",
+		row = 0,
+		col = 1,
+	},
+	yadm = {
+		enable = false,
 	},
 })
 
