1ff1fbada4bd3195783d3d5a983ca29eb4f6747b
DimasPradana <deemaazbaek@gmail.com>
2021-12-16 11:56:15 +0700

    edit gitsigns config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,13 +1,23 @@
 -- vim.cmd([[ packadd commented ]])
 
 require("gitsigns").setup({
-  signs = {
-    add          = { hl = "SignAdd",    text = "▎" },
-    change       = { hl = "SignChange", text = "▎" },
-    delete       = { hl = "SignDelete", text = "🭻" },
-    topdelete    = { hl = "SignDelete", text = "🭶" },
-    changedelete = { hl = "SignChange", text = "▎" },
-  }
+	signs = {
+		add = { hl = "SignAdd", text = "▎" },
+		change = { hl = "SignChange", text = "▎" },
+		delete = { hl = "SignDelete", text = "🭻" },
+		topdelete = { hl = "SignDelete", text = "🭶" },
+		changedelete = { hl = "SignChange", text = "▎" },
+	},
+	watch_gitdir = {
+		interval = 1000,
+	},
+	current_line_blame = true,
+	sign_priority = 5,
+	update_debounce = 500,
+	status_formatter = nil, -- Use default
+	diff_opts = {
+		internal = true,
+	},
 })
 
 -- vim:fileencoding=utf-8:ft=lua:foldmethod=marker
