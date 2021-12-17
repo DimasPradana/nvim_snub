faa9c691c9476fb657234b7ad9be44c82db466fc
DimasPradana <deemaazbaek@gmail.com>
2021-12-17 08:21:31 +0700

    fix efm langserver

--- lua/lsp/efm.lua
+++ lua/lsp/efm.lua
@@ -1,15 +1,19 @@
 local c = require("lsp/utils")
 
-require "lspconfig".efm.setup {
-    init_options = {documentFormatting = true},
-    settings = {
-        rootMarkers = {".git/"},
-        languages = {
-            lua = {
-                {formatCommand = "lua-format -i", formatStdin = true}
-            }
-        }
-    }
-}
+require("lspconfig").efm.setup({
+	init_options = { documentFormatting = true },
+	settings = {
+		rootMarkers = { ".git/" },
+		languages = {
+			lua = {
+				{ formatCommand = "lua-format -i", formatStdin = true },
+			},
+		},
+	},
+	filetypes = { "lua" },
+	cmd = { "efm-langserver" },
+	root_dir = c.custom_cwd,
+	single_file_support = false,
+})
 
 -- vim:fileencoding=utf-8:ft=lua:foldmethod=marker
