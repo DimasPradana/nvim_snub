eef2dcf6695436a56cd1bf17f9b404312033e487
DimasPradana <deemaazbaek@gmail.com>
2021-12-23 10:22:38 +0700

    add packer compile on save or source lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,12 @@
 vim.cmd([[packadd packer.nvim]]) -- Load Packer
 
+-- {{{ automatically packer compile on save / source this plugins.lua
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]]) -- }}}
 
 return require("packer").startup(function(use)
 	-- {{{ Packer
