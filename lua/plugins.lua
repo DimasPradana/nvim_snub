d53d0d0c8f33f668a4188376ccb2cb128ac6f9fc
DimasPradana <deemaazbaek@gmail.com>
2022-01-17 06:29:35 +0700

    change cmp-nvim-lsp to update from repos folder

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -159,7 +159,8 @@ return require("packer").startup(function(use)
 		requires = {
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-nvim-lsp",
+			-- "hrsh7th/cmp-nvim-lsp",
+			"~/repos/cmp-nvim-lsp",
 			"hrsh7th/cmp-nvim-lua",
 			-- "L3MON4D3/LuaSnip",
 			-- "~/repos/LuaSnip",
