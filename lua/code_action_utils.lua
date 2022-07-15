97142a19f035a3c856d075dd0aef8fd88abec2b5
DimasPradana <deemaazbaek@gmail.com>
2022-07-15 22:09:49 +0700

    try add code_action

---
+++ lua/code_action_utils.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+local lsp_util = vim.lsp.util
+
+function M.code_action_listener()
+	local context = { diagnostics = vim.lsp.diagnostic.get_line_diagnostics() }
+	local params = lsp_util.make_range_params()
+	params.context = context
+	vim.lsp.buf_request(0, "textDocument/codeAction", params, function(err, _, result)
+		-- do something with result - e.g. check if empty and show some indication such as a sign
+		-- print("error: ", err, "hasil: ", result)
+		-- require("notify")(result, "info")
+	end)
+end
+
+return M
